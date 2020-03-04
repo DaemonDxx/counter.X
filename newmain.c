5904fb32abde5c22a7faadf93c00f03efe0e9bc6
DaemonDxx <i79535999238@gmail.com>
2020-03-04 19:13:24 +0700

    init

---
+++ newmain.c
@@ -0,0 +1,118 @@
+#define _XTAL_FREQ 16000000 
+// CONFIG1
+#pragma config FOSC = INTOSCIO  // Oscillator Selection bits (INTOSCIO oscillator: I/O function on RA4/CLKO pin, I/O function on RA5/CLKI)
+#pragma config WDTE = OFF       // Watchdog Timer Enable bit (WDT enabled)
+#pragma config PWRTE = OFF      // Power-up Timer Enable bit (PWRT disabled)
+#pragma config MCLRE = ON       // RA3/MCLR/VPP Pin Function Select bit (RA3/MCLR/VPP pin function is MCLR; Weak pull-up enabled.)
+#pragma config CP = OFF         // Flash Program Memory Code Protection bit (Code protection off)
+#pragma config BOREN = ON       // Brown-out Reset Enable bits (Brown-out Reset enabled)
+#pragma config PLLEN = ON       // INTOSC PLLEN Enable Bit (INTOSC Frequency is 16 MHz (32x))
+
+// CONFIG2
+#pragma config WRTEN = OFF      // Flash memory self-write protection bits (Write protection off)
+
+// #pragma config statements should precede project file includes.
+// Use project enums instead of #define for ON and OFF.
+
+#include <xc.h>
+
+
+#define CR_LIMIT 2559
+#define LIGHT_LIMIT 3
+#define TIMER0_LIMIT 9
+#define TIMER2_LIMIT 600000
+
+int turn_flag = 0;
+int light_counter = 0;
+long interrupt_counter = 0;
+int timer0_conter = 0;
+long timer2_counter = 0;
+int prescaler = 1;
+int cr_limit = 2559;
+
+void turn() {
+    if (RC1) {
+        PORTC = 0b00000000;
+        __delay_us(1);
+        PORTC = 0b00000101;
+    } else {
+        PORTC = 0b00000000;
+        __delay_us(1);
+        PORTC = 0b00000010;
+    }
+}
+
+void initPIN() {
+    TRISA = 0b00110100;
+    TRISC = 0b00000000;
+    ANSELC = 0x00;
+    ANSELA = 0x00;
+    PORTCbits.RC0 = 1;
+}
+
+void initInterrupt() {
+    IOCA = 0b00110100;
+    GIE = 1;
+    PEIE = 1;
+    RABIE = 1;
+}
+
+void initTimer0() {
+    OPTION_REG = 0b11010100;
+}
+
+
+void setup() {
+    initPIN();
+    initInterrupt();
+    initTimer0();
+
+}
+
+
+void blink() {
+    //???????? ???? ?? 92 ??
+    PORTCbits.RC0 = 0;
+    TMR0IE = 1;
+    TMR0 = 0;
+}
+
+
+void interrupt isr() {  
+    if (RABIF) {
+        interrupt_counter++;
+        if (interrupt_counter == cr_limit) {
+            interrupt_counter = 0;
+            light_counter++;
+            //????????? ??????, ???????? ???????? ????
+            blink();
+            //???? ???? ??????? ???-?? ??? - ???????? ??????? ????????
+            if (light_counter == LIGHT_LIMIT) {
+                turn_flag = 1;
+                light_counter = 0;
+            }
+        }
+        RABIF = 0;
+    }
+    
+    if (TMR0IF && TMR0IE) {
+        timer0_conter++;
+        if (timer0_conter == TIMER0_LIMIT) {
+            PORTCbits.RC0 = 1;
+            TMR0IE = 0;
+            timer0_conter = 0;
+        }
+        TMR0IF = 0;
+    }
+    
+}
+
+void main(void) {
+    setup();
+    while (1) {
+        if (turn_flag) {
+            turn();
+            turn_flag = 0;
+        }
+    }
+}
